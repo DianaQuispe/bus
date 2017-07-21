abfeaa76acd5b9b51305df8754e8423d385e9eb5
MyDiana <danaalienmas@gmail.com>
2017-07-20 22:41:22 -0500

    bus

--- busnuevo/asientos.js
+++ busnuevo/asientos.js
@@ -5,12 +5,28 @@ var todos = [];
 var celdas = document.getElementsByTagName('td');
 for (var i = 0; i < celdas.length; i++) {
     celdas[i].addEventListener('click',redirect,false);
+    celdas[i].addEventListener("click", cambiar_color_over,false);
+    celdas[i].addEventListener("click", cambiar_color_out);
+
 }
 
 function redirect(event){
 document.getElementById("div").innerHTML=(event.target.textContent);
 }
 /////
+
+
+function cambiar_color_over(event){ 
+
+   celda.style.backgroundColor="#66ff33";
+
+ }
+
+ function cambiar_color_out(event){ 
+   celda.style.backgroundColor="blue"; 
+}   
+  
+
 
 
 
@@ -23,6 +39,22 @@ function Persona(nombre, apellido, dni) {
 
 var persona = new Persona(nombre, apellido, dni);
 todos.push(persona);
+
+function busca() {
+	//var x = document.getElementById("textito");
+	var buscar = document.getElementById("textito").value;
+	for(var i =0; i <todos.length; i++) {
+		if( buscar == "todos.dni"){
+			alert("asd");
+		}else alert("todos[i][1]");
+	}
+
+}
+
+var reservar = document.getElementById("reservar");
+var lista = document.getElementById("listar");
+
+
 // reservar.innerHTML = todos;
 
 function sera() {
@@ -30,18 +62,11 @@ function sera() {
 var nombre = document.getElementById('nombre').value;
 var apellido =  document.getElementById("apellido").value;
 var dni = document.getElementById("dni").value;
-var reservar = document.getElementById("reservar");
 var div = document.getElementById("div");
-div.innerHTML+="Nombre: " + nombre + "<br>" + "Apellido: " + apellido + "<br>" + "DNI: " + dni + "<br>" ;
+div.innerHTML+= "<div>" + "Nombre: " + nombre + "<br>" + "Apellido: " + apellido + "<br>" + "DNI: " + dni + "<br>" + "<br>" + "</div>" ;
 
 }
 
-var buscarDNI = document.getElementById("buscarDNI");
-if(buscarDNI == todos.dni) {
-	alert("sale");
-}
 
-
-var lista = document.getElementById("listar");
 
 
